6dbb5b5a444bfbc640ba5047c45f8864bb624113
Sandeep Reddy Modugu <modugusandy@gmail.com>
2026-03-30 17:28:09 +0200

    Template Method Design pattern Practice

--- Application/Src/main.cpp
+++ Application/Src/main.cpp
@@ -4,15 +4,17 @@
 #include "../../DesignPatterns/Inc/ObserverPattern.hpp"
 #include "../../DesignPatterns/Inc/StatePattern.hpp"
 #include "../../DesignPatterns/Inc/ChainofResponsibilityPattern.hpp"
+#include "../../DesignPatterns/Inc/TemplateMethodDesignPattern.hpp"
 
 int main()
 {
     std::cout << "Hello World!" << std::endl;
-    testChainofResponsibilityPattern();
-    // testStatePattern();
-    //  testObserverPattern();
-    //   testCommandPattern();
-    //   vectorDataTypePractice();
+    testTemplateMethodDesignPattern();
+    // testChainofResponsibilityPattern();
+    //  testStatePattern();
+    //   testObserverPattern();
+    //    testCommandPattern();
+    //    vectorDataTypePractice();
     getchar();
     return 0;
 }
